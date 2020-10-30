3ea0e925a7062de1793aaf8d3fc74983383e2a4b
Danny Wamuya <dannywamuya@gmail.com>
2020-10-30 12:48:31 +0300

    Add DB and DatabaseRule classes

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,32 +1,36 @@
 package models;
 
+import org.junit.Rule;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class AnimalTest {
 
-    public Animal createNewAnimal(){
+    private Animal createNewAnimal(){
         return new Animal("Zebra");
     }
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
 
     @Test
     public void animal_InstantiatesCorrectly_true() {
         Animal testAnimal = createNewAnimal();
-        assertEquals(true, testAnimal instanceof Animal);
+        assertTrue(testAnimal instanceof Animal);
     }
 
     @Test
-    public void Animal_instantiatesWithType_NonEndangered() {
+    public void animal_InstantiatesWithType_NonEndangered() {
         Animal testAnimal = createNewAnimal();
         assertEquals("Non-Endangered", testAnimal.getType());
     }
 
     @Test
-    public void equals_returnsTrueIfNameAndTypeAreSame_true() {
+    public void equals_ReturnsTrueIfNameAndTypeAreSame_true() {
         Animal testAnimal = createNewAnimal();
         Animal anotherAnimal = createNewAnimal();
-        assertTrue(testAnimal.equals(anotherAnimal));
-        assertTrue(testAnimal.animalType.equals(anotherAnimal.animalType));
+        assertEquals(testAnimal, anotherAnimal);
+        assertEquals(testAnimal.animalType, anotherAnimal.animalType);
     }
 }
---
+++ src/test/java/models/DatabaseRule.java
@@ -0,0 +1,23 @@
+package models;
+
+import models.DB;
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "danny", "newpsqlpass");
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deletePersonsQuery = "DELETE FROM animals*;";
+            con.createQuery(deletePersonsQuery).executeUpdate();
+        }
+    }
+
+}
