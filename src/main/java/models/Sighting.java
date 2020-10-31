a2e9c0c7624f3edfcaf434ce64aed484c570b76d
Danny Wamuya <dannywamuya@gmail.com>
2020-10-31 08:38:32 +0300

    Add Sighting time and corresponding tests for correct instantiation with correct time.

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,5 +1,9 @@
 package models;
 
+import org.sql2o.Connection;
+
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Objects;
 
 public class Sighting {
@@ -7,12 +11,42 @@ public class Sighting {
     private int animal_id;
     private int ranger_id;
     private int location_id;
-    private int createdAt;
+    private Timestamp created_at;
 
     public Sighting(int animal_id, int ranger_id, int location_id){
         this.animal_id = animal_id;
         this.ranger_id = ranger_id;
         this.location_id = location_id;
+    }
+
+    public static List<Sighting> getAll() {
+        String sql = "SELECT * FROM sightings;";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Sighting.class);
+        }
+    }
+
+    public static Sighting findById(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings WHERE id = :id";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sighting.class);
+        }
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO sightings (animal_id, ranger_id, location_id, created_at) VALUES (:animal_id, :ranger_id, :location_id, now())";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("animal_id", this.animal_id)
+                    .addParameter("ranger_id", this.ranger_id)
+                    .addParameter("location_id", this.location_id)
+                    .executeUpdate()
+                    .getKey();
+        }
     }
 
     public int getId() { return id; }
@@ -23,22 +57,22 @@ public class Sighting {
 
     public int getLocation_id() { return location_id; }
 
-    public int getCreatedAt() { return createdAt; }
+    public Timestamp getCreated_At() { return created_at; }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Sighting sighting = (Sighting) o;
-        return id == sighting.id &&
-                animal_id == sighting.animal_id &&
-                ranger_id == sighting.ranger_id &&
-                location_id == sighting.location_id &&
-                createdAt == sighting.createdAt;
+        return getId() == sighting.getId() &&
+                getAnimal_id() == sighting.getAnimal_id() &&
+                getRanger_id() == sighting.getRanger_id() &&
+                getLocation_id() == sighting.getLocation_id();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, animal_id, ranger_id, location_id, createdAt);
+        return Objects.hash(getId(), getAnimal_id(), getRanger_id(), getLocation_id());
     }
+
 }
--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -3,6 +3,9 @@ package models;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class SightingTest {
@@ -18,6 +21,20 @@ public class SightingTest {
     public void sighting_instantiatesCorrectly_true() {
         Sighting testSighting = createNewSighting();
         assertEquals(true, testSighting instanceof Sighting);
+        assertEquals(1, testSighting.getAnimal_id());
+        assertEquals(1, testSighting.getRanger_id());
+        assertEquals(1, testSighting.getLocation_id());
+    }
+
+    @Test
+    public void sighting_InstantiatesWithSightingTime() {
+        Sighting testSighting = createNewSighting();
+        testSighting.save();
+        Timestamp rightNow = new Timestamp(System.currentTimeMillis());
+        rightNow.setNanos(0);
+        Timestamp sightTime = Sighting.findById(testSighting.getId()).getCreated_At();
+        sightTime.setNanos(0);
+        assertEquals(rightNow, sightTime);
     }
 
     @Test
@@ -25,6 +42,41 @@ public class SightingTest {
         Sighting testSighting = createNewSighting();
         Sighting anotherSighting = createNewSighting();
         assertEquals(testSighting, anotherSighting);
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfSighting_true() {
+        Sighting firstSighting =  createNewSighting();
+        firstSighting.save();
+        Sighting secondSighting = new Sighting(2,2,2);
+        secondSighting.save();
+        assertTrue(Sighting.getAll().get(0).equals(firstSighting));
+        assertTrue(Sighting.getAll().get(1).equals(secondSighting));
+        assertEquals(2, Sighting.getAll().size());
+    }
+
+    @Test
+    public void save_SuccessfullyAddsSightingToDatabaseList() {
+        Sighting testSighting =  createNewSighting();
+        testSighting.save();
+        assertTrue(Sighting.getAll().get(0).equals(testSighting));
+    }
+
+    @Test
+    public void save_assignsIdToSighting() {
+        Sighting testSighting =  createNewSighting();
+        testSighting.save();
+        Sighting savedSighting = Sighting.getAll().get(0);
+        assertEquals(testSighting.getId(), savedSighting.getId());
+    }
+
+    @Test
+    public void find_returnsSightingWithSameId_secondSighting() {
+        Sighting firstSighting = createNewSighting();
+        firstSighting.save();
+        Sighting secondSighting = new Sighting(2,2,2);
+        secondSighting.save();
+        assertEquals(Sighting.findById(secondSighting.getId()), secondSighting);
     }
 
 }
