ecd11bf9e0d12dc1c6b85f15c200b34d21b89eec
Danny Wamuya <dannywamuya@gmail.com>
2020-10-31 22:41:32 +0300

    Set up and test App database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import models.Animal;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -16,7 +17,6 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             return new  ModelAndView(model,"index.hbs");
         }, new HandlebarsTemplateEngine());
-
 
     }
 }
--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,7 +1,32 @@
 package models;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sql2o.Sql2o;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "danny", "newpsqlpass");
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    private static String dblink = "postgres://localhost:5432/wildlife_tracker";
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI(dblink);
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "danny" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "newpsqlpass" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
