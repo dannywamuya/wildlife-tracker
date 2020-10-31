4d927c8958c677b0055815c27adf1ecfae423362
Danny Wamuya <dannywamuya@gmail.com>
2020-10-31 20:47:18 +0300

    Add google fonts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,11 +10,13 @@ import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
-//        staticFileLocation("/public");
+        staticFileLocation("/public");
 
         get("/",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new  ModelAndView(model,"index.hbs");
         }, new HandlebarsTemplateEngine());
+
+
     }
 }
